331af5cb74eccaaedc397eb45b834afef14690bd
acca-atl <zheng.guangzhi@acca-web.org>
2026-08-17 03:16:24 +0000

    v6

--- public/firebase-config.js
+++
@@ -1,8 +0,0 @@
-window.firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "acca-web-main.firebaseapp.com",
-  projectId: "acca-web-main",
-  storageBucket: "acca-web-main.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
-};
